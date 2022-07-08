ed31cbc4891350bf2286e30a4b9fb9e1775a4d52
owinog <owingunawardena0123@gmail.com>
2022-07-08 22:12:52 +0530

    socket added

---
+++ script.js
@@ -0,0 +1,58 @@
+window.onload = function () {
+    let backendIP = "";
+    let GlobalVoltage = 0;
+    while (backendIP == "") {
+        backendIP = prompt("Enter IP");
+    }
+
+    const socket = new WebSocket("ws://" + backendIP + "/8000");
+
+    socket.addEventListener("open", function (event) {
+        socket.send("Connection Established");
+    });
+
+    socket.addEventListener("message", function (event) {
+        GlobalVoltage = event.data;
+    });
+
+    var dps = []; // dataPoints
+    var chart1 = new CanvasJS.Chart("chart1Container", {
+        data: [
+            {
+                type: "line",
+                dataPoints: dps,
+            },
+        ],
+    });
+
+    var xVal = 0;
+    var yVal = 0;
+    var updateInterval = 50;
+    var dataLength = 100; // number of dataPoints visible at any point
+
+    var updateChart = function (count) {
+        count = count || 1;
+
+        for (var j = 0; j < count; j++) {
+            yVal = GlobalVoltage;
+            dps.push({
+                x: xVal,
+                y: yVal,
+            });
+            xVal++;
+        }
+
+        if (dps.length > dataLength) {
+            dps.shift();
+        }
+
+        chart1.render();
+
+        document.getElementById("val1").innerHTML = yVal;
+    };
+
+    updateChart(dataLength);
+    setInterval(function () {
+        updateChart();
+    }, updateInterval);
+};
